30ec740c1e597ad03e693c63c71f6aaaff74ea41
Shadab Akhtar <43212251+shadabakhtar97@users.noreply.github.com>
2023-09-11 09:07:37 +0530

    Add files via upload

---
+++ govariable.go
@@ -0,0 +1,13 @@
+// Here's a simple example that demonstrates variable declaration and usage in Go:
+package main
+
+import "fmt"
+
+func main() {
+    var age int
+    age = 30
+
+    name := "Alice"
+
+    fmt.Printf("Name: %s, Age: %d\n", name, age)
+}
